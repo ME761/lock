6bfc795defa3280a486d21dd548c5cb9268a00be
Amit Saharan <asaharan812@gmail.com>
2015-07-06 22:43:57 +0530

    Counter clockwise working with one edge case

--- js/me.js
+++ js/me.js
@@ -6,39 +6,50 @@ app.controller('mainCtrl',function($scope,$timeout) {
 		return z*Math.PI/180;
 	}
 
-	$scope.icurrent=360*360+75;
+	$scope.icurrent=360*360+25;
 	$scope.ocurrent=360*360+60;
 	var ocurrent=$scope.ocurrent%360,icurrent=$scope.icurrent%360,next=0,CW=1,CCW=-1,xicurrent=0;
 	$scope.DIR=CW;
 	$scope.nnjoy=function(pos){
 		next=(pos*30 )% 360;
-		var normalized=next;
-		console.log(normalized);
-		if(next<ocurrent){
-			normalized=360 - ocurrent + next;
-			console.log('yp',normalized)
-		}else{
-			normalized=pos*30 - ocurrent;
-			console.log('npe')
-		}
+		var normalized;
 		ocurrent=$scope.ocurrent%360;
 		icurrent=$scope.icurrent%360;
 		xicurrent=0;
-		console.log('normalized',normalized)
-		for(var i=ocurrent;i<=ocurrent+normalized;i++){
-			// console.log('yeah');
-			if($scope.DIR==CW) {
-				console.log(icurrent,i);
+		if($scope.DIR==CW) {
+			if(next<ocurrent){
+				normalized=360 - ocurrent + next;
+			}else{
+				normalized=pos*30 - ocurrent;
+			}
+			for(var i=ocurrent;i<=ocurrent+normalized;i++){
 				if(icurrent - (i%360)==1){
-					// xicurrent=normalized+1;
 					xicurrent=normalized - icurrent + ocurrent+1;
-					console.log('yeah',xicurrent,next+1)
 				}
 			}
+			$scope.icurrent+=xicurrent;
+			// icurrent=xicurrent;
+			$scope.ocurrent+=normalized;
+			// ocurrent=next;
 		}
-		$scope.icurrent+=xicurrent;
-		icurrent=xicurrent;
-		$scope.ocurrent+=normalized;
-		ocurrent=next;
+		if($scope.DIR==CCW){
+			if(next<ocurrent){
+				normalized=ocurrent - next;
+			}else{
+				normalized=360 - next + ocurrent;
+			}
+			// console.log(normalized);
+			for(var i=ocurrent;i>=ocurrent - normalized;i--){
+				console.log(i,ocurrent,icurrent)
+				if(i%360 - icurrent ==1){
+					xicurrent=normalized - (-icurrent + ocurrent) +1;
+					console.log('yomen',xicurrent,normalized,icurrent,ocurrent);
+				}
+			}
+			$scope.icurrent-=xicurrent;
+			// icurrent=xicurrent;
+			$scope.ocurrent-=normalized;
+			// ocurrent=next;
+		}
 	}
 });
