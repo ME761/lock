2a09bedf4186f722d7e7fbe64aa39bf55105f1ab
Amit Saharan <asaharan812@gmail.com>
2015-07-06 17:08:42 +0530

    Animation working with buttons

--- js/me.js
+++ js/me.js
@@ -1,12 +1,18 @@
 var app=angular.module('me',[]);
-app.controller('mainCtrl',function($scope) {
-	$scope.oangle=10;
-	$scope.iangle=50;
+app.controller('mainCtrl',function($scope,$timeout) {
+	$scope.oangle=0;
+	$scope.iangle=0;
 	$scope.poangle=$scope.oangle;
 	var CCW=-1,CW=1;
+	$scope.DIR=1;
+	var dir=CW;
+
+	$scope.Math=Math;
+	$scope.buttons=[1,2,3,4,5,6,7,8,9,10,11,12];
+
 	$scope.enjoy=function(){
 		rotatei();
-		console.log(direction());
+		// console.log(direction());
 	}
 
 	function rotatei(){
@@ -31,5 +37,32 @@ app.controller('mainCtrl',function($scope) {
 		}
 		return 0;
 	}
+	var pi=Math.PI;
+	$scope.torad=function(z){
+		return z*pi/180;
+	}
 	
+	$scope.gotot=function(angle){
+
+
+		if($scope.oangle <angle*30 ){
+			for (var i = $scope.oangle; i <= angle*30; i++) {
+				$timeout(function(){
+					$scope.oangle++;
+					$scope.enjoy();
+					$scope.$apply();
+				},500);	
+			}
+			$scope.oangle%=360;
+			$scope.enjoy();
+		}else{
+			for (var i = $scope.oangle; i <= 30*angle+360; i++) {
+					$timeout(function(){
+					$scope.oangle++;
+					$scope.enjoy();
+					$scope.$apply();
+				},500);	
+				};	
+		}
+	}
 });
