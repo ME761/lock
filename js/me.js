d5d3f9ec57ef61316745db29eaffc4bf10d8eb99
Amit Saharan <asaharan812@gmail.com>
2015-07-06 15:46:12 +0530

    working fine

--- js/me.js
+++ js/me.js
@@ -1,1 +1,35 @@
 var app=angular.module('me',[]);
+app.controller('mainCtrl',function($scope) {
+	$scope.oangle=10;
+	$scope.iangle=50;
+	$scope.poangle=$scope.oangle;
+	var CCW=-1,CW=1;
+	$scope.enjoy=function(){
+		rotatei();
+		console.log(direction());
+	}
+
+	function rotatei(){
+		var oangle=$scope.oangle,iangle=$scope.iangle;
+		if(touch(oangle)==touch(iangle)){
+			$scope.iangle-=direction();
+		}	
+	}
+	
+	function touch(v){
+		return (360*360+v)%360;
+	}
+	
+	function direction(){
+		var oangle=$scope.oangle,poangle=$scope.poangle;
+		$scope.poangle=$scope.oangle;
+		if(oangle-poangle>0){
+			return CCW;//CCW
+		}
+		if(oangle-poangle<0){
+			return CW;
+		}
+		return 0;
+	}
+	
+});
