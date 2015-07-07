a88dfc61ca4fdfe772f640d02efdaaee0b69a45f
Amit Saharan <asaharan812@gmail.com>
2015-07-07 19:04:00 +0530

    scramble added

--- js/me.js
+++ js/me.js
@@ -1,15 +1,22 @@
 var app=angular.module('me',[]);
 app.controller('mainCtrl',function($scope,$timeout) {
+	var ocurrent=$scope.ocurrent%360,icurrent=$scope.icurrent%360,next=0,CW=1,CCW=-1,xicurrent=0;
 	$scope.Math=Math;
 	$scope.buttons=[1,2,3,4,5,6,7,8,9,10,11,12];
 	$scope.torad=function(z){
 		return z*Math.PI/180;
 	}
 
-	$scope.icurrent=360*360+25;
-	$scope.ocurrent=360*360+30;
-	var ocurrent=$scope.ocurrent%360,icurrent=$scope.icurrent%360,next=0,CW=1,CCW=-1,xicurrent=0;
-	$scope.DIR=CW;
+	$scope.reset=function(){
+		init();
+	}
+	init();
+	function init(){
+		$scope.icurrent=360*360+getRandomArbitrary(150,350);
+		$scope.ocurrent=360*360+getRandomArbitrary(1,151);
+		$scope.unlocked=false;
+		$scope.DIR=CW;
+	}
 	$scope.nnjoy=function(pos){
 		next=(pos*30 )% 360;
 		var normalized;
@@ -20,11 +27,12 @@ app.controller('mainCtrl',function($scope,$timeout) {
 			if(next<ocurrent){
 				normalized=360 - ocurrent + next;
 			}else{
-				normalized=pos*30 - ocurrent;
+				normalized=next - ocurrent;
 			}
 			for(var i=ocurrent;i<=ocurrent+normalized;i++){
 				if(icurrent - (i%360)==1){
 					xicurrent=normalized - icurrent + ocurrent+1;
+					xicurrent%=360;
 				}
 			}
 			$scope.icurrent+=xicurrent;
@@ -35,7 +43,6 @@ app.controller('mainCtrl',function($scope,$timeout) {
 				normalized=ocurrent - next;
 			}else{
 				normalized=(360 - next + ocurrent)%360;
-				console.log(normalized,next,ocurrent)
 			}
 
 			var move=0;
@@ -46,10 +53,24 @@ app.controller('mainCtrl',function($scope,$timeout) {
 			}
 			if(move<normalized){
 				xicurrent=normalized - (-icurrent + ocurrent) +1;
+				// xicurrent=normalized - move +1;
 				xicurrent%=360;
 			}
 			$scope.icurrent-=xicurrent;
 			$scope.ocurrent-=normalized;
+		}
+		isUnlocked();
+	}
+
+	function getRandomArbitrary(min, max) {
+	    return Math.floor(Math.random() * (max - min) + min);
+	}
+
+	function isUnlocked(){
+		if($scope.icurrent%360<182 && $scope.icurrent%360>178 && (180+$scope.ocurrent)%360<182 && (180+$scope.ocurrent)%360>178){
+			$scope.unlocked=true;
+		}else{
+			$scope.unlocked=false;
 		}
 	}
 });
